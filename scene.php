d76340d33ac716df2c3c3acd6dcfaf32466f3b84
Martijn Kerpentier <martijnkerpentier162339@horizoncollege.nl>
2022-06-02 15:10:47 +0200

    website demo
    Co-authored-by: Mebracers <Mebracers@users.noreply.github.com>
    Co-authored-by: JurreP/SP_JurreX <SPJurreX@users.noreply.github.com>
    Co-authored-by: Ssionn <163021@users.noreply.github.com>

---
+++ scene.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De vragen</title>
+</head>
+<body>
+    <h1><!-- vraagnummer dynamisch bepaald: js/php --></h1>
+    <p><!-- omschrijving scenario --></p>
+
+    <a><!-- optie 1 --></a>
+    <a><!-- optie 2 --></a>
+    <?php
+    // als alle drie scenario's afgelopen zijn, dan is het einde van de test
+    
+    ?>
+    <!-- de gekozen opties worden getoond -->
+</body>
+</html>
