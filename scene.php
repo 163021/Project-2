6d0c669ca6e9237309745d2ec18024ea8bb81b6f
Martijn Kerpentier <martijnkerpentier162339@horizoncollege.nl>
2022-06-08 13:59:22 +0200

    current version
    Co-authored-by: Ssionn <163021@users.noreply.github.com>

--- scene.php
+++ scene.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>De vragen</title>
 </head>
-<body>
-    <h1><!-- vraagnummer dynamisch bepaald: js/php --></h1>
-    <p><!-- omschrijving scenario --></p>
 
-    <a><!-- optie 1 --></a>
-    <a><!-- optie 2 --></a>
+<body>
+    <h1>
+        <!-- vraagnummer dynamisch bepaald: js/php -->
+    </h1>
     <?php
+
+    ?>
+    <form method="post" action="">
+    <p>1: Je zit in de auto en bent aan het rijden. Je rijdt op de linker baan en iemand voor je snijd je af om ook naar links te gaan.</p>
+
+    <input type="radio" name="optie1" value="optie1">Optie 2: Je wordt (boos) en toeterd naar diegene voor je.</input>
+    <input type="radio" name="optie2" value="optie2">Optie 2: Je wordt (boos) en toeterd naar diegene voor je.</input>
     // als alle drie scenario's afgelopen zijn, dan is het einde van de test
-    
+    <?php
     ?>
     <!-- de gekozen opties worden getoond -->
 </body>
+
 </html>
