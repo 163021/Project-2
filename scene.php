8fec2c2b7287d81258a10f159b4934926e627cb2
Casper Kizewski <163021@student.horizoncollege.nl>
2022-06-09 11:58:58 +0200

    Een scenario afgemaakt, nog 2 te gaan

--- scene.php
+++ scene.php
@@ -5,13 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>De vragen</title>
+    <title>Scenario1</title>
 </head>
 
 <body>
-    <h1>
-        <!-- vraagnummer dynamisch bepaald: js/php -->
-    </h1>
     <?php
     /**
      * PHP version 8.1.2
@@ -22,70 +19,58 @@
      * @license  https://opensource.org/licenses/MIT MIT License
      * @link     http://localhost/
      */
+    ?>
+    <h1>
+        Scenario 1:
+    </h1>
+    <?php
+    session_start();
+    if (!isset($_POST['optie1']) && !isset($_POST['optie2'])) {
+        ?>
+        <form method="post">
+            <label for="optie1">
+                <h3><strong>Je krijgt ruzie met
+                        een man op straat en hij heeft een machette
+                        op zijn heup en dreigt je er mee te kappen.
+                        Wat doe je?</strong></h3>
+                <select id="optie1" name="optie1">
+                    <option value="veilig">Je probeert de situatie
+                        te deëscaleren</option>
+                    <option value="onveilig">Je wilt het gevecht aan en
+                        geeft hem een kaakstoot</option>
+                </select>
+                <br><br><br>
+                <input type="submit" value="Selecteren" id="btnSubmit">
+        </form>
+        <?php
+    }
+    if (isset($_POST['optie1'])) {
+        $_SESSION['optie1'] = $_POST['optie1'];
+        if ($_SESSION['optie1'] == "veilig") {
+            ?>
+            <h3><strong>Goed Gedaan. Je koos er voor om met de man te praten.
+                    Hij heeft naar je geluisterd.</strong></h3>
+                    <form method="post" action="scene2.php">
+                        <input type="submit" value="Volgende Scenario" 
+                        id="btnSubmit2">
+                    </form>
+            <?php
+        } else if ($_SESSION['optie1'] == "onveilig") {
+            ?>
+            <h3><strong>Dit gaat heel fout. De man heeft zijn machette
+                 getrokken. Hij kapt je neer. :(</strong></h3>
+                 <h3><strong>Probeer opnieuw.</strong></h3>
+            <?php
+        }
+        ?>  
+        <?php
+    }
+    ?>
+    <br><br><br>
+    <form method="post" action="#">
+        <input type="submit" value="Terug gaan">
 
-    if (!isset($_POST['submit'])) {
-        ?>
-        <form method="post" action="" value="1ste">
-            <p></p>
-            <input type="radio" name="optie1" value="optie1">Optie 1: </input>
-            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-            <input type="submit" name="submit" value="Volgende"></input>
-        </form>
-        <?php
-    }
-    ?>
-    <?php
-    if (isset($_POST['submit'])) {
-        ?>
-        <form method="post" action="" value="2de">
-            <input type="radio" name="optie1" value="optie1">Optie 1: </input>
-            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-            <input type="submit" name="submit2" value="Volgende"></input>
-        </form>
-        <?php
-    }
-    ?>
-    <?php
-    if (!isset($_POST['submit2'])) {
-        ?>
-        <form method="post" action="" value="2de">
-            <input type="radio" name="optie3" value="optie3">Optie 1: </input>
-            <input type="radio" name="optie4" value="optie4">Optie 2: </input>
-            <input type="submit" name="submit2" value="Volgende"></input>
-        </form>
-        <?php
-    }
-    ?>
-    <?php
-    if (isset($_POST['submit2'])) {
-        ?>
-        <form method="post" action="" value="3de">
-            <input type="radio" name="optie3" value="optie3">Optie 1: </input>
-            <input type="radio" name="optie4" value="optie4">Optie 2: </input>
-            <input type="submit" name="submit3" value="Volgende"></input>
-        </form>
-        <?php
-    }
-    ?>
-    <?php
-    if (!isset($_POST['submit3'])) {
-        ?>
-        <form method="post" action="" value="3de">
-            <input type="radio" name="optie5" value="optie5">Optie 1: </input>
-            <input type="radio" name="optie6" value="optie6">Optie 2: </input>
-            <input type="submit" name="submit3" value="Volgende"></input>
-        </form>
-        <?php
-    }
-    ?>
-    <?php
-    if (isset($_POST['submit3'])) {
-        ?>
-        <p><strong>Je bent klaar met deze vragenlijst!</strong></p>
-        <?php
-    }
-    ?>
-    <!-- de gekozen opties worden getoond -->
+    </form>
 </body>
 
 </html>
