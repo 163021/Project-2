c7efdb7c46c5bfa3e2b2afe278d9a1b0c99cc632
Casper Kizewski <163021@student.horizoncollege.nl>
2022-06-09 09:12:10 +0200

    Doc comment gefixed! let er maar niet op, is alleen om de error te fixen!

--- scene.php
+++ scene.php
@@ -13,6 +13,16 @@
         <!-- vraagnummer dynamisch bepaald: js/php -->
     </h1>
     <?php
+    /**
+     * PHP version 8.1.2
+     *
+     * @category Template_Class
+     * @package  Template_Class
+     * @author   Author <163021@student.horizoncollege.nl>
+     * @license  https://opensource.org/licenses/MIT MIT License
+     * @link     http://localhost/
+     */
+
     if (!isset($_POST['submit'])) {
         ?>
         <form method="post" action="" value="1ste">
@@ -27,22 +37,22 @@
     <?php
     if (isset($_POST['submit'])) {
         ?>
-    <form method="post" action="" value="2de">
-        <input type="radio" name="optie1" value="optie1">Optie 2: </input>
-        <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-        <input type="submit" name="submit2" value="Volgende"></input>
-    </form>
+        <form method="post" action="" value="2de">
+            <input type="radio" name="optie1" value="optie1">Optie 2: </input>
+            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
+            <input type="submit" name="submit2" value="Volgende"></input>
+        </form>
         <?php
     }
     ?>
     <?php
     if (!isset($_POST['submit2'])) {
         ?>
-    <form method="post" action="" value="2de">
-        <input type="radio" name="optie1" value="optie1">Optie 2: </input>
-        <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-        <input type="submit" name="submit2" value="Volgende"></input>
-    </form>
+        <form method="post" action="" value="2de">
+            <input type="radio" name="optie1" value="optie1">Optie 2: </input>
+            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
+            <input type="submit" name="submit2" value="Volgende"></input>
+        </form>
         <?php
     }
     ?>
@@ -50,10 +60,10 @@
     if (isset($_POST['submit2'])) {
         ?>
         <form method="post" action="" value="3de">
-        <input type="radio" name="optie1" value="optie1">Optie 2: </input>
-        <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-        <input type="submit" name="submit3" value="Volgende"></input>
-    </form>
+            <input type="radio" name="optie1" value="optie1">Optie 2: </input>
+            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
+            <input type="submit" name="submit3" value="Volgende"></input>
+        </form>
         <?php
     }
     ?>
@@ -61,10 +71,10 @@
     if (!isset($_POST['submit3'])) {
         ?>
         <form method="post" action="" value="3de">
-        <input type="radio" name="optie1" value="optie1">Optie 2: </input>
-        <input type="radio" name="optie2" value="optie2">Optie 2: </input>
-        <input type="submit" name="submit3" value="Volgende"></input>
-    </form>
+            <input type="radio" name="optie1" value="optie1">Optie 2: </input>
+            <input type="radio" name="optie2" value="optie2">Optie 2: </input>
+            <input type="submit" name="submit3" value="Volgende"></input>
+        </form>
         <?php
     }
     ?>
