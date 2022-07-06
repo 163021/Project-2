6c99deb43372ef37d34eff1e83fb8e7bf53810f3
Casper Kizewski <163021@student.horizoncollege.nl>
2022-07-06 12:07:22 +0200

    Scene.php Fix

--- Php/scene.php
+++ Php/scene.php
@@ -31,33 +31,35 @@
 <body>
 
     <div id="Alpha">
-        <h2 class="">*ALPHA - WIP</h2>
+        <h2 class="alpha">*ALPHA - WIP</h2>
     </div>
-    <h1 class="">
+    <h1 class="hoi">
         Scenario 1:
     </h1>
     <?php
     session_start();
     if (!isset($_POST['optie1'])) {
-        ?>
-        <form method="post" class="">
-            <label for="optie1">
-                <h3 class="">Je krijgt ruzie met
+    ?>
+        <form method="post">
+            <div class="situatie">
+                <label for="optie1">
+                    <h3 class="">Je krijgt ruzie met
                         een jongeren op straat en hij heeft een mes
                         op zijn heup en dreigt je er mee te steken.</h3>
-                <div class="">
-                    <select id="optie1" class="" name="optie1">
-                        <option value="veilig">Je probeert de situatie
-                            te deëscaleren</option>
-                        <option value="onveilig">Je wilt het gevecht aan en
-                            geeft hem een kaakstoot</option>
-                        <option value="veilig2"></option>
-                        <option value="onveilig3"></option>
-                    </select>
-                </div>
-                <div class="">
-                    <input type="submit" class="" value="Volgende">
-                </div>
+            </div>
+            <div class="">
+                <select id="optie1" class="" name="optie1">
+                    <option value="veilig">Je probeert de situatie
+                        te deëscaleren</option>
+                    <option value="onveilig">Je wilt het gevecht aan en
+                        geeft hem een kaakstoot</option>
+                    <option value="veilig2">test3</option>
+                    <option value="onveilig2">test4</option>
+                </select>
+            </div>
+            <div class="">
+                <input type="submit" class="" value="Volgende">
+            </div>
         </form>
         <br><br><br>
         <?php
@@ -65,23 +67,23 @@
     if (isset($_POST['optie1'])) {
         $_SESSION['optie1'] = $_POST['optie1'];
         if ($_SESSION['optie1'] == "veilig") {
-            ?>
-            <div class="">
+        ?>
+            <div class="hoi">
                 <h2>Goed Gedaan. Je koos er voor om met de man te praten.
                     Hij heeft naar je geluisterd.</h3>
             </div>
             <br><br>
-            <div class="">
+            <div class="hoi">
                 <h3 class="">Scenario 1 Voltooid</h1>
             </div>
             <br><br>
-            <div class="">
+            <div class="hoi">
                 <h4 class="">Redirecting in <span id="countdowntimer">5</span>...</h4>
             </div>
-            <?php
+        <?php
             header("Refresh:5; url=/Php/scene2.php", true, 303);
         } else if ($_SESSION['optie1'] == "onveilig") {
-            ?>
+        ?>
             <div>
                 <h3>Dit gaat heel fout. De man heeft zijn mes
                     getrokken. Hij heeft je neer gestoken. :(
@@ -97,11 +99,11 @@
                     <input type="submit" class="" value="Opnieuw">
                 </form>
             </div>
-            <?php
+        <?php
         } else if ($_SESSION['optie1'] == "veilig2") {
-            ?>
+        ?>
             <div>
-                <h2>Jullie proberen het uit te praten maar de man is nog niet helemaal </h3>
+                <h2>test3</h2>
             </div>
             <br><br>
             <div>
@@ -111,10 +113,28 @@
             <div>
                 <h4>Redirecting in <span id="countdowntimer">5</span>...</h4>
             </div>
-            <?php
-        }
-        ?>
         <?php
+            header("Refresh:5; url=/Php/scene2.php", true, 303);
+        } else if ($_SESSION['optie1'] == "onveilig2") {
+        ?>
+            <div>
+                <h2>Jullie vechten het uit, hij heeft je gestoken, maar je hebt hem terug gepakt. De Politie is gebeld en haalt jullie uit elkaar.</h2>
+            </div>
+            <br><br>
+            <div>
+                <h3>Probeer opnieuw.</h3>
+            </div>
+            <br><br>
+            <div>
+                <form method="post" action="#">
+                    <input type="submit" class="" value="Opnieuw">
+                </form>
+            </div>
+            <br><br>
+
+    <?php
+
+        }
     }
     ?>
     <br><br><br>
